4004361973ce209b4bbe87cc21623d22724f0221
AXT-AyaKoto <148830788+AXT-AyaKoto@users.noreply.github.com>
2025-09-20 06:41:52 +0900

    多分テンプレート型を使っていいと思うんですが

--- src/binary-search.mjs
+++ src/binary-search.mjs
@@ -1,3 +1,8 @@
+/**
+ * このモジュールは、ソート済み配列に対して二分探索を行うメソッド群を提供します。
+ * C++のSTLにある同名の関数にインスパイアされています。
+ * @module binary-search
+ */
 // @ts-check
 
 /**
@@ -14,9 +19,10 @@ const DEFAULT_COMPARE_FN = (a, b) => {
 /**
  * 配列`array`に`target`と等しい値が存在するかどうかを、二分探索を用いて判定します。
  * (時間計算量: $O(\log n)$)
- * @param {any[]} array - `compareFn`を用いてソート済みの配列
- * @param {any} target - 探索する値
- * @param {(a: any, b: any) => number} [compareFn] - 比較に用いる関数。デフォルト値はArray#sort()と同じ。
+ * @template T
+ * @param {T[]} array - `compareFn`を用いてソート済みの配列
+ * @param {T} target - 探索する値
+ * @param {(a: T, b: T) => number} [compareFn] - 比較に用いる関数。デフォルト値はArray#sort()と同じ。
  * @returns {boolean} `target`と等しい値が`array`に存在する場合は`true`、存在しない場合は`false`
  */
 const binary_search = (array, target, compareFn = DEFAULT_COMPARE_FN) => {
@@ -39,9 +45,10 @@ const binary_search = (array, target, compareFn = DEFAULT_COMPARE_FN) => {
  * 配列`array`の中で、`target`以上と判定される最初の要素のインデックスを返します。
  * (時間計算量: $O(\log n)$)
  *
- * @param {any[]} array - `compareFn`を用いてソート済みの配列
- * @param {any} target - 探索する値
- * @param {(a: any, b: any) => number} [compareFn] - 比較に用いる関数。デフォルト値はArray#sort()と同じ。
+ * @template T
+ * @param {T[]} array - `compareFn`を用いてソート済みの配列
+ * @param {T} target - 探索する値
+ * @param {(a: T, b: T) => number} [compareFn] - 比較に用いる関数。デフォルト値はArray#sort()と同じ。
  * @returns {number} `target`以上と判定される最初の要素のインデックス。`array`内に`target`以上の要素が存在しない場合は`array.length`を返します。
  */
 const lower_bound = (array, target, compareFn = DEFAULT_COMPARE_FN) => {
@@ -63,9 +70,10 @@ const lower_bound = (array, target, compareFn = DEFAULT_COMPARE_FN) => {
  * 配列`array`の中で、`target`より大きいと判定される最初の要素のインデックスを返します。
  * (時間計算量: $O(\log n)$)
  *
- * @param {any[]} array - `compareFn`を用いてソート済みの配列
- * @param {any} target - 探索する値
- * @param {(a: any, b: any) => number} [compareFn] - 比較に用いる関数。デフォルト値はArray#sort()と同じ。
+ * @template T
+ * @param {T[]} array - `compareFn`を用いてソート済みの配列
+ * @param {T} target - 探索する値
+ * @param {(a: T, b: T) => number} [compareFn] - 比較に用いる関数。デフォルト値はArray#sort()と同じ。
  * @returns {number} `target`より大きいと判定される最初の要素のインデックス。`array`内に`target`より大きい要素が存在しない場合は`array.length`を返します。
  */
 const upper_bound = (array, target, compareFn = DEFAULT_COMPARE_FN) => {
