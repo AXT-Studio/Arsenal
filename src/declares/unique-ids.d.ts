8f273639b9982063e6c62a0f2a242eca50972852
AXT-AyaKoto <148830788+AXT-AyaKoto@users.noreply.github.com>
2025-09-20 19:33:47 +0900

    各データ構造および関数に対する詳細なJSDocコメントを追加

--- src/declares/unique-ids.d.ts
+++ src/declares/unique-ids.d.ts
@@ -1,1 +1,6 @@
+/**
+ * UUIDv7を生成します。
+ * @param {number} [timestamp] - UNIXタイムスタンプ(ミリ秒単位)。省略すると現在の時刻が使用されます。
+ * @returns {string} - 生成されたUUIDv7文字列
+ */
 export function getUUIDv7(timestamp?: number): string;
