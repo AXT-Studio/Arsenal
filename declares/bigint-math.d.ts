aeb1ba576fde0e5d46b3cbce9648ca36463a30f7
AXT-AyaKoto <148830788+AXT-AyaKoto@users.noreply.github.com>
2025-11-14 10:16:25 +0900

    ExMathじゃなくてBigIntMathのほうがよくないですか？

--- declares/bigint-math.d.ts
+++ declares/bigint-math.d.ts
@@ -5,5 +5,9 @@ export declare class BigIntMath {
     /**
      * nの整数平方根を返す
      */
-    static sqrt(n: bigint): bigint;
+    static isqrt(n: bigint): bigint;
+    /**
+     * 整数`a`, 非負整数`n`, 正整数`m`について、`a ** n % m`を求めます。
+     */
+    static modPow(a: bigint, n: bigint, m: bigint): bigint;
 }
--- documents/articles/.vitepress/sidebar.mjs
+++ documents/articles/.vitepress/sidebar.mjs
@@ -65,6 +65,7 @@ const sidebar = [
                     {
                         text: 'class: BigIntMath', link: '/specs/bigint-math/BigIntMath/', collapsed: true, items: [
                             { text: 'static method: isqrt', link: '/specs/bigint-math/BigIntMath/isqrt' },
+                            { text: 'static method: modPow', link: '/specs/bigint-math/BigIntMath/modPow' },
                         ]
                     },
                 ]
