69c2494144ff5b656fac7b32bd02b35347452488
AXT-AyaKoto <148830788+AXT-AyaKoto@users.noreply.github.com>
2025-12-06 12:37:52 +0900

    declare消していいよな別に 消します

--- declares/next-permutation.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 呼び出されるたびに配列の次の順列を返すジェネレーター関数。`for...of`構文で回すことで順列全列挙が可能。
- * - 時間計算量: O(N! * N) (※Nは配列の要素数。各順列生成がO(N)、順列の総数がN!)
- * @generator
- */
-export function next_permutation<T>(arr: T[]): Generator<T[], void, unknown>;
